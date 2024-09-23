7413e52b326c66d3f9a5576b5045d5f0f5e4afb5
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-09-23 10:20:45 -0300

    finsh

--- chart_project/src/components/BarChart.jsx
+++ chart_project/src/components/BarChart.jsx
@@ -24,15 +24,6 @@ ChartJS.register(
 function BarChart({ selectedDay }) {
   const chartRef = useRef(null);
 
-  const getSituacaoValue = (situacao) => {
-    switch (situacao) {
-      case 'vermelho': return 50;
-      case 'amarelo': return 30;
-      case 'verde': return 10;
-      default: return 0;
-    }
-  };
-
   const filterByDayOfWeek = (data, dayOfWeek) => {
     return data.filter((entry) => {
       const date = new Date(entry.datetime);
@@ -40,55 +31,55 @@ function BarChart({ selectedDay }) {
     });
   };
 
-  const calculateAverageSituacao = (data) => {
+  const calculateRealQuantidade = (data) => {
     const groupedData = {};
-    
+
     data.forEach((entry) => {
       const [datePart, timePart] = entry.datetime.split(' ');
       const [hour, minute] = timePart.split(':');
       const roundedMinute = Math.floor(minute / 10) * 10;
-      const timeKey = ` ${hour}:${roundedMinute.toString().padStart(2, '0')}`;
+      const timeKey = `${hour}:${roundedMinute.toString().padStart(2, '0')}`;
 
       if (!groupedData[timeKey]) {
-        groupedData[timeKey] = { total: 0, count: 0 };
+        groupedData[timeKey] = { totalQuantidade: 0, count: 0 };
       }
 
-      groupedData[timeKey].total += getSituacaoValue(entry.situacao);
+      groupedData[timeKey].totalQuantidade += entry.quantidade;
       groupedData[timeKey].count += 1;
     });
 
-    const averagedData = Object.keys(groupedData).map((timeKey) => {
-      const avgValue = groupedData[timeKey].total / groupedData[timeKey].count;
+    const resultData = Object.keys(groupedData).map((timeKey) => {
+      const avgQuantidade = groupedData[timeKey].totalQuantidade / groupedData[timeKey].count;
       return {
         datetime: timeKey,
-        situacao: avgValue,
+        quantidade: avgQuantidade,
       };
     });
 
-    return averagedData;
+    return resultData;
   };
 
   const [chartData, setChartData] = useState({
     labels: [],
     datasets: [{
-      label: "Gráfico Pessoas nas filas", 
+      label: "Quantidade de Pessoas nas Filas",
       data: [],
-      backgroundColor: [], 
+      backgroundColor: [],
       borderColor: 'transparent',
-      borderRadius: 50, 
+      borderRadius: 50,
     }]
   });
 
   useEffect(() => {
     const interval = setInterval(() => {
       const filteredData = filterByDayOfWeek(UserData, selectedDay);
-      const averagedData = calculateAverageSituacao(filteredData);
+      const realData = calculateRealQuantidade(filteredData);
 
       const chart = chartRef.current;
       const ctx = chart?.ctx;
 
       if (ctx) {
-        const gradientColors = averagedData.map(() => {
+        const gradientColors = realData.map(() => {
           const gradient = ctx.createLinearGradient(0, 0, 0, 400);
           gradient.addColorStop(0, 'rgba(125,131,137,1)'); // Cor clara no topo
           gradient.addColorStop(1, 'rgba(46,48,51,1)');   // Cor escura no final
@@ -96,13 +87,13 @@ function BarChart({ selectedDay }) {
         });
 
         setChartData({
-          labels: averagedData.map((data) => data.datetime),
+          labels: realData.map((data) => data.datetime),
           datasets: [{
-            label: "Gráfico Pessoas nas filas", 
-            data: averagedData.map((data) => data.situacao),
+            label: "Quantidade de Pessoas nas Filas",
+            data: realData.map((data) => data.quantidade),
             backgroundColor: gradientColors,
             borderColor: 'transparent',
-            borderRadius: 5, 
+            borderRadius: 5,
           }]
         });
       }
@@ -114,48 +105,39 @@ function BarChart({ selectedDay }) {
   const chartOptions = {
     plugins: {
       legend: {
-        display: false, 
+        display: false,
       },
       title: {
         display: true,
-        text: 'Gráfico Pessoas nas filas x Horário',
+        text: 'Quantidade de Pessoas nas Filas x Horário',
       },
     },
     scales: {
       x: {
         grid: {
-          display: false, 
-          drawOnChartArea: false, 
-          color: 'rgba(200, 200, 200, 0.2)', 
-          tickLength: 18,
+          display: false,
+          drawOnChartArea: false,
+          color: 'rgba(200, 200, 200, 0.2)',
+          tickLength: 5,
         },
         ticks: {
-          maxRotation: 0, 
-          minRotation: 0, 
-          autoSkip: false, 
-          
-          
+          maxRotation: 0,
+          minRotation: 0,
+          autoSkip: false,
         },
       },
       y: {
         grid: {
-          display: true, 
-          drawOnChartArea: true, 
+          display: true,
+          drawOnChartArea: true,
           drawTicks: false,
-          
         },
         beginAtZero: true,
       }
     },
     layout: {
-      backgroundColor: '#2e2e2e', 
-      
-     
-      
-
+      backgroundColor: '#2e2e2e',
     },
-    
-    
   };
 
   return <Bar ref={chartRef} data={chartData} options={chartOptions} />;
